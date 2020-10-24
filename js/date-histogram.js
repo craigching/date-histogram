5f79462b86d33e82c170672008621e1c0c5a8bff
Craig Ching <craigching@gmail.com>
2020-10-23 20:10:53 -0500

    Uses traditional margin convention

--- js/date-histogram.js
+++ js/date-histogram.js
@@ -10,49 +10,33 @@ const dateHistogram = (props) => {
 
     console.log(data);
 
+    const margin = {top: 30, right: 10, bottom: 50, left: 50};
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+
     const yAccessor = d => d.count;
     const xAccessor = d => d.date;
 
-    let dimensions = {
-        width: svgWidth, height: svgHeight, margin: {
-            top: 30,
-            right: 10,
-            bottom: 50,
-            left: 50
-        }
-    };
-
-    dimensions.boundedWidth = dimensions.width
-        - dimensions.margin.left
-        - dimensions.margin.right;
-
-    dimensions.boundedHeight = dimensions.height
-        - dimensions.margin.top
-        - dimensions.margin.bottom;
-
     const wrapper = d3.select(container)
         .append('svg')
-        .attr('width', dimensions.width)
-        .attr('height', dimensions.height);
+        .attr('width', width)
+        .attr('height', height);
 
     const bounds = wrapper.append('g')
-        .style('transform', `translate(${dimensions.margin.left
-            }px, ${dimensions.margin.top
-            }px)`);
+        .style('transform', `translate(${margin.left}, ${margin.top})`);
 
     const xScale = d3.scaleBand()
-        // .domain(d3.extent(data, xAccessor))
         .domain(data.map(d => d.date))
-        .range([0, dimensions.boundedWidth])
+        .range([0, width])
         .padding(0.05);
 
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(data, yAccessor)])
-        .range([dimensions.boundedHeight, 0]);
+        .range([height, 0]);
 
     const xAxisGroup = bounds.append('g')
         .attr('class', 'x axis')
-        .attr('transform', `translate(0, ${dimensions.boundedHeight})`);
+        .attr('transform', `translate(0, ${height})`);
 
     const yAxisGroup = bounds.append('g')
         .attr('class', 'y axis');
@@ -69,7 +53,7 @@ const dateHistogram = (props) => {
         .join('rect')
         .attr('y', d => yScale(yAccessor(d)))
         .attr('x', d => xScale(xAccessor(d)))
-        .attr('height', d => dimensions.boundedHeight - yScale(yAccessor(d)))
+        .attr('height', d => height - yScale(yAccessor(d)))
         .attr('width', xScale.bandwidth)
         .attr('fill', 'lightseagreen');
 };
